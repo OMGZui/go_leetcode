774433e6637ede736b95f3eef9702b1a914d3112
小粽子 <862275679@qq.com>
2018-08-13 17:11:58 +0800

    ✅2018-08-13 17:11:57✅

---
+++ ch01数组/26RemoveDuplicatesFromSortedArray.go
@@ -0,0 +1,45 @@
+/*
+ * Created by GoLand.
+ * User: 小粽子
+ * Date: 2018/8/13
+ * Time: 16:28
+ */
+package main
+
+import "fmt"
+
+func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	i := 0
+	for j := 1; j < len(nums); j++ {
+		if nums[j] != nums[i] {
+			i++
+			nums[i] = nums[j]
+		}
+	}
+	return i + 1
+}
+func removeDuplicatesP(nums []int) (int, []int, []int) {
+	if len(nums) == 0 {
+		return 0, nums, nums
+	}
+	i := 0
+	for j := 1; j < len(nums); j++ {
+		if nums[j] != nums[i] {
+			i++
+			nums[i] = nums[j]
+		}
+	}
+	return i + 1, nums[:i+1], nums
+}
+
+func main() {
+	nums := []int{0,0,1,1,1,2,2,3,3,4}
+	fmt.Println(removeDuplicates(nums))
+	
+	numsP := []int{0,0,1,1,1,2,2,3,3,4}
+	n, num, _num := removeDuplicatesP(numsP)
+	fmt.Println(n, num, _num)
+}
