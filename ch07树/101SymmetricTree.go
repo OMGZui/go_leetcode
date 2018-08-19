f6b55f1f61b3492ea250066291a0dc01e903b0d1
小粽子 <862275679@qq.com>
2018-08-19 16:23:07 +0800

    ✅2018-08-19 16:23:07✅

--- ch07树/101SymmetricTree.go
+++ ch07树/101SymmetricTree.go
@@ -6,3 +6,35 @@
  */
 package main
 
+import (
+	"fmt"
+	"../leetcode.tool"
+)
+
+type TreeNode2 = leetcode_tool.TreeNode
+
+func isSymmetric(root *TreeNode2) bool {
+	if root == nil {
+		return true
+	}
+
+	return recur(root.Left, root.Right)
+
+}
+
+func recur(left *TreeNode2, right *TreeNode2) bool {
+	if left == nil && right == nil {
+		return true
+	}
+
+	if left == nil || right == nil {
+		return false
+	}
+
+	return left.Val == right.Val && recur(left.Left, right.Right) && recur(left.Right, right.Left)
+}
+
+func main() {
+	q := leetcode_tool.Ints2TreeNode([]int{1, 2, 2, 3, 4, 4, 3})
+	fmt.Println(isSymmetric(q))
+}
